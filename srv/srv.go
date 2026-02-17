acf67c93268a7499ff63da317806d1ad4cacf34a
jesc7 <jesc@mail.ru>
2026-02-17 12:58:53 +0400

    --- Auto Git Commit ---

--- srv/srv.go
+++ srv/srv.go
@@ -134,12 +134,11 @@ type Msg struct {
 }
 
 type Client struct {
-	key   string
-	pwd   string
-	offer string
-	conn  *websocket.Conn
-	busy  bool
-	//prev      MessageType
+	key       string
+	pwd       string
+	offer     string
+	conn      *websocket.Conn
+	busy      bool
 	heartbeat int64
 }
 
